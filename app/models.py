5a29763f16b1e3fa94e30c8a3082b5eaef1b3858
Wandy Olivares <125208383+wandy-w@users.noreply.github.com>
2023-04-06 07:23:20 -0400

    Modelos Creados

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+      nombre = models.CharField(max_length=50)
+      numero = models.IntegerField(max_length=100)
+      
+      def __str__(self):
+          return self.nombre
+      
+    
+class Registros(models.Model):
+      nombre = models.CharField(max_length=50)
+      
+      def __str__(self):
+          return self.nombre
+
+
+
+class Amigos(models.Model):
+      models.ForeignKey(User, on_delete=models.CASCADE)
+      amigos = models.CharField( max_length=50)
